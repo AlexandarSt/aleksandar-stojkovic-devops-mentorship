85c418ea2e12dc695ecff9c20fb5e0fe63af1110
Aleksandar Stojkovic <alexstweb@gmail.com>
2023-03-14 13:08:37 +0100

    Week-3 task

---
+++ week-3/README.md
@@ -0,0 +1,7 @@
+## TASK-3: Bash / Shell scripting
+
+All srcipts can be found in:
+`week-3/scripts`
+
+Screenshots are in:
+`week-3/screenshots`
---
+++ week-3/scripts/chapter15/members.sql
@@ -0,0 +1,2 @@
+INSERT INTO members (lname,fname,address,city,state,zip) VALUES
+('Stojkovic', 'Aleksandar', 'Nemanjina13', 'Beograd', 'Srbija', '11000');
